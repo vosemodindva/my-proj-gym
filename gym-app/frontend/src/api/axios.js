e83f0a4956c97e14a8bcb3a92c31945864db7103
cybergrenade <georgy.anonimus@yandex.ru>
2025-06-22 01:11:37 +0300

    On branch feature/docker  Changes to be committed: 	modified:   backend/Dockerfile 	modified:   backend/GymApp/urls.py 	modified:   docker-compose.yml 	modified:   frontend/Dockerfile 	modified:   frontend/src/api/axios.js

--- gym-app/frontend/src/api/axios.js
+++ gym-app/frontend/src/api/axios.js
@@ -1,9 +1,17 @@
 import axios from "axios";
 
+
 const api = axios.create({
   baseURL: "http://localhost:8000/api/",
 });
 
+
+/*
+const API = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  withCredentials: true,
+});
+*/
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("access");
   if (token) {
